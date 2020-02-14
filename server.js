aa786fa6a777a3fc1df7a0fc1deb3f7b6604b284
brer4449 <58540640+brer4449@users.noreply.github.com>
2020-02-13 22:15:22 -0500

    add an app.js in public folder, and fixed server.js as well as added to views folder

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const mysql = require("mysql");
 
 const app = express();
+// Set the port of our application
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static("public"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+
+app.use(express.static("public"));
 
 const routes = require("./controllers/burgers_controllers.js");
 app.use(routes);
@@ -16,6 +22,91 @@ app.get("/", function(req, res) {
   res.json(path.join(__dirname, "public/index.html"));
 });
 
-app.listen(PORT, function() {
-  console.log("Server is listening on: http://localhost:" + PORT);
+// Syncing our sequelize models and then starting our Express app
+db.sequelize.sync().then(function() {
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
 });
+
+/*
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "veryeasypassword",
+  database: "moviePlannerDB"
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+// Use Handlebars to render the main index.html page with the movies in it.
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM movies;", function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.render("index", { movies: data });
+  });
+});
+
+// Create a new movie
+app.post("/api/movies", function(req, res) {
+  connection.query(
+    "INSERT INTO movies (movie) VALUES (?)",
+    [req.body.movie],
+    function(err, result) {
+      if (err) {
+        return res.status(500).end();
+      }
+
+      // Send back the ID of the new movie
+      res.json({ id: result.insertId });
+      console.log({ id: result.insertId });
+    }
+  );
+});
+
+// Update a movie
+app.put("/api/movies/:id", function(req, res) {
+  connection.query(
+    "UPDATE movies SET movie = ? WHERE id = ?",
+    [req.body.movie, req.params.id],
+    function(err, result) {
+      if (err) {
+        // If an error occurred, send a generic server failure
+        return res.status(500).end();
+      } else if (result.changedRows === 0) {
+        // If no rows were changed, then the ID must not exist, so 404
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
+});
+
+// Delete a movie
+app.delete("/api/movies/:id", function(req, res) {
+  connection.query("DELETE FROM movies WHERE id = ?", [req.params.id], function(
+    err,
+    result
+  ) {
+    if (err) {
+      // If an error occurred, send a generic server failure
+      return res.status(500).end();
+    } else if (result.affectedRows === 0) {
+      // If no rows were changed, then the ID must not exist, so 404
+      return res.status(404).end();
+    }
+    res.status(200).end();
+  });
+});
+*/
